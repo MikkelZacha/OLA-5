c3817009227b36b899a2bc754d9689c51186cde7
NicolaiLoeve <cph-nn219@stud.ek.dk>
2025-12-06 14:56:03 +0100

    Update AFW API collect script with new API key

--- afw_project/scripts/afw_collect.R
+++ afw_project/scripts/afw_collect.R
@@ -6,7 +6,7 @@ library(DBI)
 library(RMySQL)
 
 # Din API key - HUSK Bearer foran
-api_key <- "Bearer SG_APIM_616QEBX83TYEECB5XRTWH5G5GRXRV949BWG54HBKAJTK6FHJ1VW0"
+api_key <- "Bearer APIAPIAPI"
 
 # Logfil
 log_file <- "C:/afw_project/logs/afw_collect.log"
@@ -62,7 +62,7 @@ try({
   con <- dbConnect(
     RMySQL::MySQL(),
     user = "root",
-    password = "Tfx58kzd.EK.Stud.dk",
+    password = "KODEKODEKODE", <- ENS EGEN KODE
     host = "127.0.0.1",
     dbname = "salling_afw",
     port = 3306
@@ -112,3 +112,4 @@ if (is.null(con)) {
 }
 
 log_msg("Script færdigt")
+
